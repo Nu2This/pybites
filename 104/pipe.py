d00dff0d94ee5588bde1079d43bdeab5e0bc8298
Michael Heidelberger <mheide@nu2home>
2020-07-05 21:05:52 -0500

    104 submission

---
+++ 104/pipe.py
@@ -0,0 +1,17 @@
+message = """Hello world!
+We hope that you are learning a lot of Python.
+Have fun with our Bites of Py.
+Keep calm and code in Python!
+Become a PyBites ninja!"""
+
+def split_in_columns(message=message):
+    """Split the message by newline (\n) and join it together on '|'
+       (pipe), return the obtained output string"""
+    thing = message.split('\n')
+    pipe = '|'
+    finish = pipe.join(thing)
+    return finish
+    pass
+
+if __name__ == '__main__':
+    split_in_columns(message)
