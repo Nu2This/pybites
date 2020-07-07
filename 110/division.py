dcb6d10b6d96c8cabcae4f7a10f7a3591aff0015
Michael Heidelberger <mheide@desktop-mheidelberger.NVC>
2020-07-07 10:56:45 -0500

    110 sub

---
+++ 110/division.py
@@ -0,0 +1,21 @@
+def divide_numbers(numerator, denominator):
+    """For this exercise you can assume numerator and denominator are of type
+       int/str/float.
+       Try to convert numerator and denominator to int types, if that raises a
+       ValueError reraise it. Following do the division and return the result.
+       However if denominator is 0 catch the corresponding exception Python
+       throws (cannot divide by 0), and return 0"""
+    try:
+        n = int(numerator)
+        d = int(denominator)
+
+        print(n/d)
+        return n/d
+
+    except ValueError:
+        raise ValueError
+    except ZeroDivisionError:
+        return 0
+
+if __name__ == '__main__':
+    divide_numbers(1.5, 0)
