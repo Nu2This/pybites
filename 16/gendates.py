aa90a5f125b91dd3a5b7f1279a5fe496b80a2caa
Michael Heidelberger <mheide@desktop-mheidelberger.NVC>
2020-07-08 14:35:56 -0500

    16 sub

--- 16/gendates.py
+++ 16/gendates.py
@@ -2,21 +2,43 @@ from datetime import datetime, timedelta
 from itertools import islice
 
 PYBITES_BORN = datetime(year=2016, month=12, day=19)
-out = PYBITES_BORN
+start = PYBITES_BORN
 
 
 def gen_special_pybites_dates():
+    y = 0
+    while True:
+        global PYBITES_BORN
+        global start
+        PYBITES_BORN += timedelta(days=100)
+        if PYBITES_BORN - start > timedelta(days=365):
+            y +=1
+            start = start + timedelta(days=365)
+            yield start
+        yield PYBITES_BORN
 
-    global out
+       # yield PYBITES_BORN
+#    global PYBITES_BORN
+#    for i in range(n):
 
-    out += timedelta(days=100)
-    print(out.items())
-    return out
-    pass
+#        PYBITES_BORN += timedelta(days=100)
+#        yield PYBITES_BORN
+
+
+   # global start
+   # out = start + timedelta(days=100)
+   # for i in range(10):
+   #     out += timedelta(days=100)
+
+   #     yield out
+   # pass
 
 if __name__ == '__main__':
-    def runfunction():
-        for x in range(10):
-            gen_special_pybites_dates()
+   # def runfunction():
+   #     for x in range(10):
+   #         gen_special_pybites_dates()
 
-    runfunction()
+   # runfunction()
+    gen = gen_special_pybites_dates()
+    dates = list(islice(gen, 10))
+    print(dates)
