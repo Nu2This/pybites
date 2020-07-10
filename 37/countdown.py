0dfa6e3863c123b2f8a20b46c31e869b43b94100
Michael Heidelberger <mheide@desktop-mheidelberger.NVC>
2020-07-10 15:21:18 -0500

    37 sub

---
+++ 37/countdown.py
@@ -0,0 +1,17 @@
+def countdown_for(start=10):
+    for i in reversed(range(1, start + 1)):
+        print(i)
+    print('time is up')
+
+
+def countdown_recursive(start=10):
+    if start > 0:
+        print(start)
+        start -= 1
+        countdown_recursive(start)
+    else:
+        print('time is up')
+    pass
+
+if __name__ == '__main__':
+    countdown_recursive()
