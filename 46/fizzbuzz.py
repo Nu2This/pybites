f533ccf0e67ec4980d0afa1d56fbff9ba263ee3a
Michael Heidelberger <mheide@desktop-mheidelberger.NVC>
2020-07-13 15:17:29 -0500

    annoyed 2

--- 46/fizzbuzz.py
+++ 46/fizzbuzz.py
@@ -1,11 +1,12 @@
-def fizzbuzz(num, ret):
-    for x in range(num):
-        if x % ret == 0:
-            return 'Fizz'
-        if x % ret == 0:
-            return 'Buzz'
+def fizzbuzz(num):
+    for x in range(num + 1):
+        print(x)
+        if x % 3 == 0:
+            print('Fizz')
+        if x % 5 == 0:
+            print('Buzz')
         else:
-            return x
+            print(x)
 
 if __name__ == '__main__':
     fizzbuzz(20)
